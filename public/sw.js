a04e93f9121a67fa47d165d96b90d5f72ce8cf9e
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-15 13:21:01 +0500

    Remove floating Report button; enlarge footer feedback text

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v8';
+const CACHE = 'grips-shell-v9';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
