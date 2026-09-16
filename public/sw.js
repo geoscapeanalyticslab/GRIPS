12c82cd789dbfa78e21ee85b9accf9a10a84fc79
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-16 17:22:05 +0500

    Revert hero emblem to ring logo; cache v21

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v20';
+const CACHE = 'grips-shell-v21';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
