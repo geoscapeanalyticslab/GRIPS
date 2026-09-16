6f3a996420dbe40b6ba017d7653e67b16d6d3062
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-16 16:13:52 +0500

    Add GRIPS primary logo in header; remove Report button; remove My Reports + Auto-Blocked from About; near-black text; cache v17

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v16';
+const CACHE = 'grips-shell-v17';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
