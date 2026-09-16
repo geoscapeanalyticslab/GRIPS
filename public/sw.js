a8a3f7e547fa9faae03f07927effb97ede7b8fec
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-16 17:17:40 +0500

    Use Pakistan-map favicon badge as hero emblem; cache v20

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v19';
+const CACHE = 'grips-shell-v20';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
