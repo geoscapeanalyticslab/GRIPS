4bbda68709bad3a4a53220a306fdb194b2dbef33
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-16 11:39:08 +0500

    Rebrand accent to Ocean Blue; mobile fit + junk-type filter; cache v13

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v10';
+const CACHE = 'grips-shell-v13';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
