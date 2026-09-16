d4ac4895c42efc10f399b2249e0fb06d6e515a5a
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-16 15:46:31 +0500

    Cleaner cards (hide empty author/abstract, demote sparse), near-black text, GSAL intro in About, Did-you-mean spelling suggestion; cache v16

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v15';
+const CACHE = 'grips-shell-v16';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
