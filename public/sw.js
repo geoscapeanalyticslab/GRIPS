bc598002413417e1f292007215704d6791766160
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-16 17:12:07 +0500

    New favicon (transparent Pakistan-map badge) and new header logo (emblem + wordmark); cache v19

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v18';
+const CACHE = 'grips-shell-v19';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
